b69fa70bc4803950c947bb304e9c067d8a434bf3
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-06 16:54:32 +0100

    add paths

--- library/digital_library/views.py
+++ library/digital_library/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+books_data = [
+    {'id': 1, 'title': 'Harry Potter'},
+    {'id': 2, 'title': 'The Meaning of Life'}
+]
+
+# books_data = [
+#     { 'id': 1, 'title': 'Life, the Universe and Everything', 'author': 'Douglas Adams'},
+#     { 'id': 2, 'title': 'The Meaning of Liff' 'author': 'Douglas Adams'},
+#     { 'id': 3, 'title': 'The No. 1 Ladies\' Detective Agency' 'author': 'Alexander McCall Smith'}
+# ]
+
 def home(req):
-    return render(req, 'home.html')
+    data = {'books': 'books_data'}
+    return render(req, 'home.html', data)
 
 # def show(req, id):
 #     return render(req, )
