7c41faf9072a5b153f4f81847ac1e21a8906511b
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-05-07 12:47:36 +0100

    Finished main authentication and protecting routes

--- library/digital_library/models.py
+++ library/digital_library/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
   title = models.CharField(max_length=200)
   author = models.CharField(max_length=100)
+  borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
   
   def __str__(self):
     return self.title
